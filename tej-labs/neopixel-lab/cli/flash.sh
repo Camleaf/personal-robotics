7f825411aaed5c36a545ca728eacb5864dc3ce81
Camleaf <bdaace09@gmail.com>
2026-05-04 10:52:15 -0400

    random stuff

--- tej-labs/neopixel-lab/cli/flash.sh
+++ tej-labs/neopixel-lab/cli/flash.sh
@@ -1,2 +1,2 @@
 arduino-cli compile --fqbn arduino:avr:uno --build-path ./build
-arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:uno --input-dir ./build
+arduino-cli upload -p /dev/ttyACM1 --fqbn arduino:avr:uno --input-dir ./build
--- tej-labs/neopixel-lab/neopixel-lab.ino
+++ tej-labs/neopixel-lab/neopixel-lab.ino
@@ -4,16 +4,36 @@
 #define LED_COUNT 4
 #define NEOPIXEL_PIN 6
 
-Adafruit_NeoPixel px(LED_COUNT, NEOPIXEL_PIN, NEO_KHZ800);
+Adafruit_NeoPixel px(LED_COUNT, NEOPIXEL_PIN, NEO_GRB);
 
 void setup(){
     px.begin();
     px.show();
-    px.fill(px.Color(255,0,0),0,NEOPIXEL_PIN);
+    for (int i = 0;i < LED_COUNT;i++){
+      px.setPixelColor(i,px.Color(255,0,0));
+    }
+  
+    px.setBrightness(100);
+    px.show();
 }
 
 
+uint32_t colList[] = {
+  px.Color(255,0,0),
+  px.Color(0,255,0),
+  px.Color(0,0,255)
+};
 
+int idx = 0;
 void loop(){
-
+     
+    delay(300);
+    for (int i = 0; i<LED_COUNT;i++){
+      px.setPixelColor(i,colList[idx]);
+      delay(100);
+      px.show();
+    }
+    px.show();
+    idx++;
+    idx %=3;
 }
