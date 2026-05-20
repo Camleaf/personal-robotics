0f79eb353bed848d53137e9a9a8edba1c496fa28
Camleaf <bdaace09@gmail.com>
2026-05-19 20:57:29 -0400

    remove legacy arcade drive

--- tej-labs/robotics-summative/src/drivetrain.h
+++ tej-labs/robotics-summative/src/drivetrain.h
@@ -9,33 +9,6 @@
 
 using namespace std;
 
-class Arcade {
-    public:
-        Arcade(uint8_t kbr1, uint8_t kbr2, uint8_t kbl1, uint8_t kbl2, uint8_t kfr1, uint8_t kfr2, uint8_t kfl1, uint8_t kfl2, int deadzone=20);
-        void setMaxSpeed(uint8_t maxSpeed); // 0-255
-        void setTurnPower(uint8_t turnPower); // 0-255
-
-        void invertMotor(int motor, bool inverted); //0 backright
-        /* JoyY is for moving forward and back, JoyX is for rotation
-         */ 
-
-        void updateMotor(int joyX, int joyY, bool slowmode, bool boost); // Must be in range (-512,512)
-        // settings
-        uint8_t maxSpeed = 255;
-        uint8_t turnPower = 255;
-        // motor pins
-        
-        uint8_t kbr1 = 0;
-        uint8_t kbr2 = 0;
-        uint8_t kbl1 = 0;
-        uint8_t kbl2 = 0;
-        uint8_t kfr1 = 0;
-        uint8_t kfr2 = 0;
-        uint8_t kfl1 = 0;
-        uint8_t kfl2 = 0;
-        int deadzone = 20;
-        array<int,4> invertDir = {1,1,1,1};
-};
 
 
 class Mecanum {
