bed002b0377fa6e6f2d1c2ff0176c12c215a5ebd
Camleaf <bdaace09@gmail.com>
2026-05-24 10:05:01 -0400

    there we go

--- tej-labs/robotics-summative/src/mpu6050.cpp
+++ tej-labs/robotics-summative/src/mpu6050.cpp
@@ -13,20 +13,39 @@ void GyroMPU6050::begin(){
     delay(500);
     Wire.beginTransmission(0x68);
     Wire.write(0x6B); 
-    Wire.write(0x00);
-    Wire.endTransmission(true);
-    // set sample rate to 19
-    Wire.beginTransmission(0x68);
-    Wire.write(0x19); 
-    Wire.write(0x09); 
-    Wire.endTransmission(true);
+    Wire.write(0x80); 
+    byte error = Wire.endTransmission();
+    
+    if (error != 0) {
+        Serial.print("err on reset:");
+        Serial.println(error);
+    } else {
+        Serial.println("no err on reset");
+    }
+    
+    delay(100);
 
-    // set gyro range to 8g
+    Wire.beginTransmission(0x68);
+    Wire.write(0x6B); 
+    Wire.write(0x00); 
+    error = Wire.endTransmission();
+    
+    if (error != 0) {
+        Serial.print("wakeup err: ");
+        Serial.println(error);
+    } else {
+        Serial.println("no err on wakeup");
+    }
+
+
     Wire.beginTransmission(0x68);
     Wire.write(0x1B); 
-    Wire.write(0x08); // 0x08 = 500 deg/s
-    Wire.endTransmission(true);
-}
+    Wire.write(0x08); 
+    error = Wire.endTransmission();
+    
+    if (error == 0) {
+        Serial.println("no error on range set");
+    }}
 
 
 
@@ -49,7 +68,14 @@ void GyroMPU6050::generate_tuned_values(){
     for (int i = 0; i < tuningCycles; i++) {
         Wire.beginTransmission(0x68);
         Wire.write(0x47); //register for GYRO_ZOUT_H
-        Wire.endTransmission();         
+        byte writeError = Wire.endTransmission(false); // Don't release the bus yet
+        
+        if (writeError != 0) {
+            Serial.print("Write Error before Read! Code: ");
+            Serial.println(writeError);
+            delay(100);
+            continue;
+        }   
         Wire.requestFrom(0x68, 2); 
         if (Wire.available() == 2) {
             int16_t rawZ = Wire.read() << 8 | Wire.read();
@@ -88,7 +114,7 @@ void GyroMPU6050::fetch_data(uint32_t timestamp){ // use esp timer to get this t
     // 500 deg per s range = 65.5 whatever units read
     float gyroZ = (rawZ) / 65.5f; 
     
-    yaw += (gyroZ) * delta;
+    yaw += (gyroZ - yawHardBias) * delta;
     
     Serial.println(yaw);
     lastTime = timestamp;
